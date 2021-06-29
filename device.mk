3e6ede16a72ad095730bf3433492818fe5da4a58
baalajimaestro <me@baalajimaestro.me>
2021-06-29 07:56:19 +0000

    X2:[SQUASH]: Rework dex flags again
    When I went through https://source.android.com/devices/tech/dalvik/configure my previous configs felt wrong, this one should be perfect (I hope).
    Even though there is a slight trade-off for boot time by using the speed profile, we do make up for it by using 8 threads.
    PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER attempts to compile all prebuilts fully optimised to speed level, thus the phone doesnt need to deal with it.
    I also do know that this might be a trade-off on size, but I do see the first boot time is more worthy to trade-off.
    The speed level mentioned here runs dex verification and compiles all AOT methods.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    
    X2: Switch certain dexopt profiles to verify
    
    Prebuilt apps like Gmail/Google App, will be updated by google play, and there is no need spend time and space optimising what is going to be replaced.
    Switch to Google's recommendation of using verify for an OTA package.
    Applications still stay on speed.
    Instead, replace the install profile with speed-profile making apps perform better.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -69,6 +69,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
