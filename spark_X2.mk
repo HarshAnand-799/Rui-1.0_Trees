de60a134e75817d7b845eee87234da3916060972
HarshAnand-799 <harshanand7898@gmail.com>
2022-01-07 16:25:56 +0000

    X2: Init SparkOS

--- spark_X2.mk
+++ spark_X2.mk
@@ -9,11 +9,11 @@ $(call inherit-product, device/realme/X2/device.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
+PRODUCT_NAME := spark_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
